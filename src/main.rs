e3d80c98b790bbcf043cb56ed2cceeb03acb6bf9
Fabiano Martins <fabiano.paula.martins@gmail.com>
2024-02-22 08:14:09 -0300

    updating code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 #[macro_use] extern crate rocket;
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world2!"
+fn hello() -> &'static str {
+    "Hello, world!"
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![hello])
+}
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::local::blocking::Client;
+    use rocket::http::Status;
+
+    #[test]
+    fn hello_world() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client.get(uri!(super::hello)).dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_string().unwrap(), "Hello, world!");
+    }
 }
