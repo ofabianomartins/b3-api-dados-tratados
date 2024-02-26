6e6ba2724c6bce295450bc5e8c0ef6422b8d642c
Fabiano Martins <fabiano.paula.martins@gmail.com>
2024-02-26 10:00:51 -0300

    updating data

--- migrations/2024-02-25-000921_create_calendars/up.sql
+++ migrations/2024-02-25-000921_create_calendars/up.sql
@@ -1,9 +1,11 @@
 -- Your SQL goes here
 
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE IF NOT EXISTS public.calendars(
   id SERIAL PRIMARY KEY, 
   name VARCHAR NOT NULL UNIQUE,
-  uuid VARCHAR NOT NULL UNIQUE,
-  created_at timestamp with time zone NOT NULL,
-  updated_at timestamp with time zone NOT NULL
+  uuid UUID NOT NULL DEFAULT uuid_generate_v4()
+--  created_at timestamp with time zone NOT NULL,
+--  updated_at timestamp with time zone NOT NULL
 )
