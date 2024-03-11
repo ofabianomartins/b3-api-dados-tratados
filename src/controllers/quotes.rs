3bead9eaec54a4def27fbd076ca2d648fce30e53
Fabiano Martins <fabiano.paula.martins@gmail.com>
2024-03-11 12:23:59 -0300

    cleaning controllers and updating gitignore

--- src/controllers/quotes.rs
+++ src/controllers/quotes.rs
@@ -2,7 +2,6 @@ use rocket::get;
 use rocket::response::status::NoContent;
 use rocket::serde::json::Json;
 
-
 use diesel::SelectableHelper;
 use diesel::RunQueryDsl;
 use diesel::query_dsl::QueryDsl;
@@ -23,7 +22,6 @@ use bigdecimal::BigDecimal;
 use crate::connections::db_connection;
 use crate::connections::redis_connection;
 use crate::models::Quote;
-// use crate::models::NewQuote;
 use crate::schema::quotes::dsl::*;
 
 #[get("/quotes")]
@@ -49,24 +47,6 @@ pub fn destroy(quote_id: i32) -> NoContent {
 #[response(status = 201, content_type = "json")]
 pub struct CreatedJson(Json<Quote>);
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct CreateParams {
-    pub date: NaiveDate,
-    pub symbol: String,
-    pub close: BigDecimal,
-	pub open: Option<BigDecimal>,
-	pub high: Option<BigDecimal>,
-	pub low: Option<BigDecimal>,
-	pub average: Option<BigDecimal>,
-	pub ask: Option<BigDecimal>,
-	pub bid: Option<BigDecimal>,
-	pub adjust: Option<BigDecimal>,
-	pub volume: Option<BigDecimal>,
-	pub trades: Option<BigDecimal>,
-}
-
-//#[post("/quotes")]
-//pub async fn create(quote_params: Json<CreateParams>) -> &'static str {
 #[post("/quotes", format="json", data = "<quote_params>")]
 pub async fn create(quote_params: &str) -> &'static str {
     let conn = &mut redis_connection();
--- src/controllers/tickers.rs
+++ src/controllers/tickers.rs
@@ -5,6 +5,7 @@ use rocket::serde::json::Json;
 
 use diesel::SelectableHelper;
 use diesel::RunQueryDsl;
+use diesel::ExpressionMethods;
 use diesel::query_dsl::QueryDsl;
 use diesel::insert_into;
 use diesel::delete;
@@ -12,22 +13,37 @@ use diesel::delete;
 use crate::connections::db_connection;
 use crate::models::Ticker;
 use crate::models::NewTicker;
-use crate::schema::tickers::dsl::*;
+use crate::schema::tickers;
 
-#[get("/tickers")]
-pub fn index() -> Json<Vec<Ticker>> {
+#[get("/tickers?<symbol>")]
+pub fn index(symbol: Option<String>) -> Json<Vec<Ticker>> {
     let conn = &mut db_connection();
-    let results = tickers
-        .select(Ticker::as_select())
-        .load(conn)
-        .expect("Error loading tickers");
+
+    let mut results: Vec<Ticker>;
+
+    match symbol {
+        Some(x) => {
+            results = tickers::dsl::tickers
+                .filter(tickers::dsl::symbol.eq(x))
+                .select(Ticker::as_select())
+                .load(conn)
+                .expect("Error loading tickers");
+        },
+        None => {
+            results = tickers::dsl::tickers
+                .select(Ticker::as_select())
+                .load(conn)
+                .expect("Error loading tickers");
+
+        }
+    }
     return Json(results);
 }
 
 #[delete("/tickers/<ticker_id>")]
 pub fn destroy(ticker_id: i32) -> NoContent {
     let conn = &mut db_connection();
-    delete(tickers.find(ticker_id))
+    delete(tickers::dsl::tickers.find(ticker_id))
         .execute(conn)
         .expect("Error loading tickers");
     return NoContent;
@@ -40,7 +56,7 @@ pub struct CreatedJson(Json<Ticker>);
 #[post("/tickers", format="json", data = "<new_ticker>")]
 pub async fn create(new_ticker: Json<NewTicker<'_>>) -> CreatedJson {
     let conn = &mut db_connection();
-    let result = insert_into(tickers)
+    let result = insert_into(tickers::dsl::tickers)
         .values(&*new_ticker)
         .returning(Ticker::as_returning())
         .get_result(conn)
